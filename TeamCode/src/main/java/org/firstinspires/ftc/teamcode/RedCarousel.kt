7ef410d2b1198f416227be4c80afc852c37bd687
ismellpillows <calvinluo8@gmail.com>
2022-01-30 17:50:37 -0800

    dsffs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedCarousel.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedCarousel.kt
@@ -19,7 +19,7 @@ class RedCarousel : BaseAutonomous() {
         drivetrain.move(y = 40)
         lift.bonus()
         sleep(500)
-        drivetrain.move(20, 46)
+        drivetrain.move(19.5, 46)
         sleep(500)
         scoring.right()
         sleep(1500)
@@ -55,21 +55,21 @@ class RedCarousel : BaseAutonomous() {
         drivetrain.move(y = 40, brake = false)
         lift.bonus()
         scoring.right()
-        drivetrain.move(20, 46)
+        drivetrain.move(19.5, 46)
         scoring.up()
         lift.down()
 
         intake.duckSuck()
         when (camera.analysis) {
-            SkystonePosition.LEFT -> drivetrain.move(x = 6)
-            SkystonePosition.CENTER -> drivetrain.move(x = -6)
+            SkystonePosition.LEFT -> drivetrain.move(x = 6, brake = false)
+            SkystonePosition.CENTER -> drivetrain.move(x = -6, brake = false)
             else -> {}
         }
         drivetrain.move(y = 35)
         intake.off()
         lift.top()
         scoring.right()
-        drivetrain.move(20, 46)
+        drivetrain.move(19, 46)
         sleep(1000)
 
         drivetrain.move(-32, 30)
