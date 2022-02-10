235fa33431db0cfd0f987e71b04876f6b7fc4feb
ismellpillows <calvinluo8@GMAIL.COM>
2022-02-09 17:23:05 -0800

    red done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuto.kt
@@ -8,18 +8,18 @@ class BlueAuto : BaseAutonomous() {
     override fun autonomous() {
         drivetrain.move(y = 14)
         sleep(1000)
-        drivetrain.move(heading = -82)
+        drivetrain.move(heading = -83)
         sleep(1000)
         drivetrain.move(x = -26, heading = -89)
 
         sleep(100)
         carousel.deliver(Alliance.BLUE)
         sleep(1000)
-        drivetrain.move(x = 28)
+        drivetrain.move(x = 25)
         lift.bonus()
         sleep(1000)
-        drivetrain.move(y = -20 + if (camera.analysis ===
-            SkystonePosition.RIGHT) 0 else 6)
+        drivetrain.move(y = -18 + if (camera.analysis ===
+            SkystonePosition.RIGHT) 0 else 3)
         sleep(1000)
         scoring.right()
         sleep(2500)
